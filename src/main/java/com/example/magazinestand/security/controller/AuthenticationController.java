8519dc0344ee071c939c66b98390cd7d35c2d0b1
Andrii Krokhta <nowayoutta13@gmail.com>
2022-01-13 10:24:21 +0200

    Added users created counter for prometheus

--- src/main/java/com/example/magazinestand/security/controller/AuthenticationController.java
+++ src/main/java/com/example/magazinestand/security/controller/AuthenticationController.java
@@ -4,6 +4,8 @@ import com.example.magazinestand.security.dto.AuthenticationResponse;
 import com.example.magazinestand.security.dto.UserDto;
 import com.example.magazinestand.security.service.CustomUserDetailsService;
 import com.example.magazinestand.security.util.JwtUtil;
+import io.prometheus.client.CollectorRegistry;
+import io.prometheus.client.Counter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -15,18 +17,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthenticationController {
 
 	private final CustomUserDetailsService userDetailsService;
-
 	private final AuthenticationManager authenticationManager;
-
 	private final JwtUtil jwtUtil;
+	private final Counter counter;
 
 	@Autowired
 	public AuthenticationController(CustomUserDetailsService userDetailsService,
 									AuthenticationManager authenticationManager,
-									JwtUtil jwtUtil) {
+									JwtUtil jwtUtil,
+									CollectorRegistry collectorRegistry) {
 		this.userDetailsService = userDetailsService;
 		this.authenticationManager = authenticationManager;
 		this.jwtUtil = jwtUtil;
+		this.counter = Counter.build()
+				.name("users_registered")
+				.help("The amount of registered users.")
+				.register(collectorRegistry);
 	}
 
 	@PostMapping("/login")
@@ -40,6 +46,7 @@ public class AuthenticationController {
 	@PostMapping("/register")
 	public AuthenticationResponse registerUserAndCreateAuthenticationToken(@RequestBody UserDto userDto) {
 		var userDetails = userDetailsService.createUser(userDto);
+		counter.inc();
 		return new AuthenticationResponse(jwtUtil.generateToken(userDetails));
 	}
 
