b47ab7db44110d6e824fea4ce153da554c4cfe5c
Andrii Krokhta <nowayoutta13@gmail.com>
2022-01-17 15:26:33 +0200

    Added oauth2

--- src/main/java/com/example/magazinestand/security/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.magazinestand.security.repository;
-
-import com.example.magazinestand.security.entity.UserEntity;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends CrudRepository<UserEntity, String> {
-}
